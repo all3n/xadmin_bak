af619b4966c81ab9fef29e0e6ad06c97de4c7ce3
all3n <wanghch8398@163.com>
2022-12-15 19:25:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xadmin
+# xadmin
+# xadmin
---
+++ xadmin-system/src/main/java/com/devhc/xadmin/AppRun.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2019-2020
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.devhc.xadmin;
+
+import io.swagger.annotations.Api;
+import com.devhc.xadmin.annotation.rest.AnonymousGetMapping;
+import com.devhc.xadmin.utils.SpringContextHolder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAsync
+@RestController
+@Api(hidden = true)
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(AppRun.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
+    }
+
+    @Bean
+    public SpringContextHolder springContextHolder() {
+        return new SpringContextHolder();
+    }
+
+    /**
+     * 访问首页提示
+     *
+     * @return /
+     */
+    @AnonymousGetMapping("/")
+    public String index() {
+        return "Backend service started successfully";
+    }
+}
